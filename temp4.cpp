ceb7198c2e79678c6cd32861649f7f1ca212b079
Gaurav Mamgain <magma@Gauravs-MacBook-Air.local>
2024-11-16 21:13:44 +0530

    new edit

--- temp4.cpp
+++ temp4.cpp
@@ -1,34 +1,13 @@
 
  
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 const int MOD=1e9+7;
 #define lil long long int
- 
-     //write your code
-  void solve(){
-    
-      int n;
-      cin>>n;
-       vector<int> arr(n,0);
-       for(int i=0;i<n;i++){
-           cin>>arr[i];
-       }
-       
-     //write your code
-
-
-
- }
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-   int t;
-   cin>>t;
-    while(t--){
-        solve();
-    }
-    
+ cout<<endl;
+cout<<"hiiiiiii"<<endl;
+return 0;
 }
  
  
